38968774be727a80668d64b3fa165fe1ba6ce23a
papun <papunmahakul@globussoft.com>
2023-07-25 18:58:24 +0530

    update contact

---
+++ app/model/contact.model.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require('mongoose');
+const mongo = require('mongoose');
+
+const contactSchema = mongo.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add the contact name"],
+          },
+          email: {
+            type: String,
+            required: [true, "Please add the contact email address"],
+          },
+          phone: {
+            type: String,
+            required: [true, "Please add the contact phone number"],
+          },
+        },
+        {
+          timestamps: true,
+        } 
+);
+
+module.exports = mongoose.model('Contact',contactSchema);
