db3729360609cc2aa0c66ed3c81906eaaa50ac3f
papun <papunmahakul@globussoft.com>
2023-07-27 18:59:58 +0530

    upadte route

--- index.js
+++ index.js
@@ -5,50 +5,23 @@ require('ejs');
 require('dotenv').config();
 const config = process.env;
 const app = express();
-// const routes = require('./app/route/general');
+const routes = require('./app/route/general.route');
 const dbConn = require('./app/config/mongodb-connect');
-dbConn.connect();
-const { createContact } = require('./app/controller/contact.controller');
-
-app.use(bodyParser.urlencoded({ extended: true }));
+dbConn.connect(); 
 
 app.listen(config.PORT,()=>{
     console.log('server runing on port '+config.PORT);
 });
 
-
-// app.use(express.static(__dirname + '/public/'));
-// console.log(__dirname + '/public');
-
-
-app.get('/', (req, res) => { 
-   res.render('home'); // it is automatically search the form.ejs file inside ./views/
-});
-
-app.get('/contact',(req,res)=>{
-    res.render('contact');
-});
-
-app.get('/login',(req,res)=>{
-    res.render('login');
-});
-
-app.get('/register',(req,res)=>{
-    res.render('register');
-});
-
-app.post('/submit',createContact);
-// app.post('/submit',(req,res)=>{
-//     let name = req.body.name;
-//     let email = req.body.email;
-    
-//     res.render('confirmation',{name,email});
-//     // console.log(req);
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views','./app/views'); 
 
+app.use(routes);
+
+// app.use(express.static(__dirname + '/public/'));
+// console.log(__dirname + '/public');
 
 
  
