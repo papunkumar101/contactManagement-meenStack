b6a08aa6f741c7a0f1808d78ce6afe20403ba9e5
papun <papunmahakul@globussoft.com>
2023-07-27 19:09:03 +0530

    update routsss

--- app/route/general.route.js
+++ app/route/general.route.js
@@ -1,33 +1,36 @@
- 
+const { createContact } = require('../controller/contact.controller'); 
 
-// const MongoDB = require('./app/config/mongodb-connect');
-// MongoDB.connect();
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => { 
-  res.render('home'); // it is automatically search the form.ejs file inside ./views/
-});
+    res.render('home'); // it is automatically search the form.ejs file inside ./views/
+ });
+ 
+ router.get('/contact',(req,res)=>{
+     res.render('contact');
+ });
+ 
+ router.get('/login',(req,res)=>{
+     res.render('login');
+ });
+ 
+ router.get('/register',(req,res)=>{
+     res.render('register');
+ });
+ 
+ router.post('/submit',createContact);
+ // router.post('/submit',(req,res)=>{
+ //     let name = req.body.name;
+ //     let email = req.body.email;
+     
+ //     res.render('confirmation',{name,email});
+ //     // console.log(req);
+ // });
 
-router.get('/contact',(req,res)=>{
-    res.render('contact');
-});
 
-router.get('/login',(req,res)=>{
-    res.render('login');
-});
-
-router.get('/register',(req,res)=>{
-    res.render('register');
-});
-
-router.post('/submit',(req,res)=>{ 
-    let name = req.body.name;
-    let email = req.body.email;
-    
-    res.render('confirmation',{name,email});
-    // console.log(req);
-});
+ module.exports = router;
 
 
  
